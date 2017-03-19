a57b53f2de13e00ba0707083dd5e3bd46e75c515
hejtmy <hejtmy@gmail.com>
2017-03-19 23:13:48 +0100

    Substracts one from marks and starts

--- format-bva-sequence.R
+++ format-bva-sequence.R
@@ -4,6 +4,8 @@ format_bva_sequence = function(ls){
     allo_ego[i] = ifelse(ls$allo_ego[i] == 1, "Ego", "Allo")
   }
   allo_ego = paste(allo_ego, collapse = '",\n "')
+  ls$starts <- sapply(ls$starts, minus_one_or_zero)
+  ls$marks <- sapply(ls$marks, minus_one_or_zero)
   marks = paste(ls$marks, collapse = ",\n")
   starts = paste(ls$starts, collapse = ",\n")
   cat(allo_ego)
@@ -11,4 +13,9 @@ format_bva_sequence = function(ls){
   cat(marks)
   print("starts")
   cat(starts)
+}
+
+minus_one_or_zero <- function(x){
+  ret = ifelse(((x-1)<0), 0, x-1)
+  return(ret)
 }
